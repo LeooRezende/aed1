824d0c6ddf1e2cb500affb172fc015952b7b2e2d
Pedro Crispim Pereira <57925151+wraphs@users.noreply.github.com>
2023-08-06 12:04:24 -0300

    Add files via upload
    lol

--- fila.c
+++ fila.c
@@ -2,14 +2,16 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <stdbool.h>
 #include "fila.h"
 
-Transacao *criarTransacao(double valor, int tipo, const char *dataHora, const char *descricao)
+Transacao *criarTransacao(int id, double valor, int tipo, const char *dataHora, const char *descricao)
 {
     Transacao *novaTransacao = (Transacao *)malloc(sizeof(Transacao));
 
     if (novaTransacao != NULL)
     {
+        novaTransacao->id = id;
         novaTransacao->valor = valor;
         novaTransacao->tipo = tipo;
         strncpy(novaTransacao->dataHora, dataHora, sizeof(novaTransacao->dataHora) - 1);
@@ -56,7 +58,7 @@ void adicionarTransacao(Fila *fila, Transacao *novaTransacao)
     }
 }
 
-Transacao *tirarTransacao(Fila *fila)
+/*Transacao *tirarTransacao(Fila *fila)
 {
     if (taVazia(fila))
     {
@@ -72,11 +74,43 @@ Transacao *tirarTransacao(Fila *fila)
         fila->tras = NULL;
     }
     return antigaTransacao;
+}*/
+
+Transacao *tirarTransacao(Fila *fila)
+{
+    if (taVazia(fila))
+    {
+        printf("Fila vazia!\n");
+        return NULL;
+    }
+
+    Transacao *anterior = NULL;
+    Transacao *atual = fila->frente;
+
+    while (atual->next != NULL)
+    {
+        anterior = atual;
+        atual = atual->next;
+    }
+
+    if (anterior == NULL)
+    {
+        fila->frente = NULL;
+        fila->tras = NULL;
+    }
+    else
+    {
+        anterior->next = NULL;
+        fila->tras = anterior;
+    }
+
+    return atual;
 }
+
 
 void mostrarHistorico(Fila *fila)
 {
-    int numero = 0;
+    int numero = 1;
 
     if (taVazia(fila))
     {
@@ -99,6 +133,7 @@ void mostrarHistorico(Fila *fila)
         //printf("           Transação nº%d           \n", numero);
         printf("Valor: R$%.2lf, Data: %.25s\n", atual->valor, atual->dataHora);
         printf("Descrição: %s\n", atual->descricao);
+        printf("ID da Transação: #%d\n", atual->id);
         atual = atual->next;
         numero++;
     }
@@ -114,4 +149,302 @@ void liberarFila(Fila *fila)
         free(antigaTransacao);
     }
     free(fila);
+}
+
+bool compararTransacoesDecrescente(Transacao *transacao1, Transacao *transacao2)
+{
+    return transacao1->valor > transacao2->valor;
+}
+
+bool compararTransacoesCrescente(Transacao *transacao1, Transacao *transacao2)
+{
+    return transacao1->valor < transacao2->valor;
+}
+
+bool compararDatasDecrescente(Transacao *transacao1, Transacao *transacao2)
+{
+    // Compare as datas das transações (use strcmp para comparar as strings de dataHora)
+    return strcmp(transacao1->dataHora, transacao2->dataHora) > 0;
+}
+
+int particionar(Transacao **transacoesArray, int esquerda, int direita, bool (*comparar)(Transacao *, Transacao *))
+{
+    Transacao *pivot = transacoesArray[direita];
+    int i = esquerda - 1;
+
+    for (int j = esquerda; j <= direita - 1; j++)
+    {
+        if (comparar(transacoesArray[j], pivot))
+        {
+            i++;
+            Transacao *temp = transacoesArray[i];
+            transacoesArray[i] = transacoesArray[j];
+            transacoesArray[j] = temp;
+        }
+    }
+
+    Transacao *temp = transacoesArray[i + 1];
+    transacoesArray[i + 1] = transacoesArray[direita];
+    transacoesArray[direita] = temp;
+
+    return i + 1;
+}
+
+
+// Função auxiliar para obter uma string representando o tipo de transação
+const char* obterTipoTransacaoString(int tipo)
+{
+    return (tipo == 1) ? "CRÉDITO" : "DÉBITO";
+}
+
+void bubbleSortDescendingTransacoes(Fila *fila)
+{
+    if (taVazia(fila))
+    {
+        printf("Fila vazia, adicione transações.\n");
+        return;
+    }
+
+    int numTransacoes = 0;
+    Transacao *atual = fila->frente;
+    while (atual != NULL)
+    {
+        numTransacoes++;
+        atual = atual->next;
+    }
+
+    Transacao **transacoesArray = (Transacao **)malloc(numTransacoes * sizeof(Transacao *));
+    if (transacoesArray == NULL)
+    {
+        printf("Erro ao alocar memória para ordenação.\n");
+        return;
+    }
+
+    atual = fila->frente;
+    for (int i = 0; i < numTransacoes; i++)
+    {
+        transacoesArray[i] = atual;
+        atual = atual->next;
+    }
+
+    for (int i = 0; i < numTransacoes - 1; i++)
+    {
+        int swapped = 0;
+        for (int j = 0; j < numTransacoes - i - 1; j++)
+        {
+            if (transacoesArray[j]->valor < transacoesArray[j + 1]->valor)
+            {
+                Transacao *temp = transacoesArray[j];
+                transacoesArray[j] = transacoesArray[j + 1];
+                transacoesArray[j + 1] = temp;
+                swapped = 1;
+            }
+        }
+
+        if (swapped == 0)
+            break;
+    }
+
+    printf("========================\nTransações Ordenadas:\n");
+    for (int i = 0; i < numTransacoes; i++)
+    {
+        printf("======================================\n");
+        printf("      Transação nº%d (%s)\n", i, obterTipoTransacaoString(transacoesArray[i]->tipo));
+        printf("Valor: R$%.2lf, Data: %.25s\n", transacoesArray[i]->valor, transacoesArray[i]->dataHora);
+        printf("Descrição: %s\n", transacoesArray[i]->descricao);
+    }
+    printf("========================\n");
+
+    free(transacoesArray);
+}
+
+void selectionSort(Transacao **transacoesArray, int numTransacoes, bool (*comparar)(Transacao *, Transacao *))
+{
+    for (int i = 0; i < numTransacoes - 1; i++)
+    {
+        int minIndex = i;
+        for (int j = i + 1; j < numTransacoes; j++)
+        {
+            if (comparar(transacoesArray[j], transacoesArray[minIndex]))
+            {
+                minIndex = j;
+            }
+        }
+
+        if (minIndex != i)
+        {
+            Transacao *temp = transacoesArray[i];
+            transacoesArray[i] = transacoesArray[minIndex];
+            transacoesArray[minIndex] = temp;
+        }
+
+    }
+}
+
+void ordenarFila(Fila *fila, bool (*comparar)(Transacao *, Transacao *))
+{
+    if (taVazia(fila))
+    {
+        printf("Fila vazia, adicione transações.\n");
+        return;
+    }
+
+    int numTransacoes = 0;
+    Transacao *atual = fila->frente;
+    while (atual != NULL)
+    {
+        numTransacoes++;
+        atual = atual->next;
+    }
+
+    Transacao **transacoesArray = (Transacao **)malloc(numTransacoes * sizeof(Transacao *));
+    if (transacoesArray == NULL)
+    {
+        printf("Erro ao alocar memória para ordenação.\n");
+        return;
+    }
+
+    atual = fila->frente;
+    for (int i = 0; i < numTransacoes; i++)
+    {
+        transacoesArray[i] = atual;
+        atual = atual->next;
+    }
+
+    selectionSort(transacoesArray, numTransacoes, comparar);
+
+    printf("========================\nTransações Ordenadas:\n");
+    for (int i = 0; i < numTransacoes; i++)
+    {
+        printf("======================================\n");
+        printf("      Transação nº%d (%s)\n", i, obterTipoTransacaoString(transacoesArray[i]->tipo));
+        printf("Valor: R$%.2lf, Data: %.25s\n", transacoesArray[i]->valor, transacoesArray[i]->dataHora);
+        printf("Descrição: %s\n", transacoesArray[i]->descricao);
+    }
+    printf("========================\n");
+
+    free(transacoesArray);
+}
+
+void quicksortTransacoesPorData(Transacao **transacoesArray, int esquerda, int direita, bool (*comparar)(Transacao *, Transacao *))
+{
+    if (esquerda < direita)
+    {
+        int indiceParticao = particionar(transacoesArray, esquerda, direita, comparar);
+
+        quicksortTransacoesPorData(transacoesArray, esquerda, indiceParticao - 1, comparar);
+        quicksortTransacoesPorData(transacoesArray, indiceParticao + 1, direita, comparar);
+    }
+}
+
+void mostrarTransacoesCredito(Fila *fila)
+{
+    if (taVazia(fila))
+    {
+        printf("Fila vazia, não há transações.\n");
+        return;
+    }
+
+    int numero = 0;
+    Transacao *atual = fila->frente;
+
+    while (atual != NULL)
+    {
+        if (atual->tipo == 1) // Verifica se é do tipo crédito (1)
+        {
+            printf("======================================\n");
+            printf("      Transação nº%d (CRÉDITO)\n", numero);
+            printf("Valor: R$%.2lf, Data: %.25s\n", atual->valor, atual->dataHora);
+            printf("Descrição: %s\n", atual->descricao);
+            numero++;
+        }
+
+        atual = atual->next;
+    }
+
+    if (numero == 0)
+    {
+        printf("Não há transações do tipo CRÉDITO.\n");
+    }
+}
+
+Transacao *buscaBinariaTransacao(Transacao **transacoesArray, int esquerda, int direita, bool (*comparar)(Transacao *, Transacao *), int tipoBuscado)
+{
+    if (direita >= esquerda)
+    {
+        int meio = esquerda + (direita - esquerda) / 2;
+
+        if (transacoesArray[meio]->tipo == tipoBuscado)
+        {
+            return transacoesArray[meio];
+        }
+
+        if (comparar(transacoesArray[meio], transacoesArray[meio + 1]) && tipoBuscado == 2)
+        {
+            return buscaBinariaTransacao(transacoesArray, esquerda, meio - 1, comparar, tipoBuscado);
+        }
+
+        return buscaBinariaTransacao(transacoesArray, meio + 1, direita, comparar, tipoBuscado);
+    }
+
+    return NULL;
+}
+
+void mostrarTransacoesDebito(Fila *fila)
+{
+    if (taVazia(fila))
+    {
+        printf("Fila vazia, não há transações.\n");
+        return;
+    }
+
+    int numTransacoes = 0;
+    Transacao *atual = fila->frente;
+
+    while (atual != NULL)
+    {
+        numTransacoes++;
+        atual = atual->next;
+    }
+
+    Transacao **transacoesArray = (Transacao **)malloc(numTransacoes * sizeof(Transacao *));
+    if (transacoesArray == NULL)
+    {
+        printf("Erro ao alocar memória para ordenação.\n");
+        return;
+    }
+
+    atual = fila->frente;
+    for (int i = 0; i < numTransacoes; i++)
+    {
+        transacoesArray[i] = atual;
+        atual = atual->next;
+    }
+
+    // Ordenar a fila por data (do mais novo ao mais antigo) para a busca binária
+    quicksortTransacoesPorData(transacoesArray, 0, numTransacoes - 1, compararDatasDecrescente);
+
+    Transacao *debitoEncontrado = buscaBinariaTransacao(transacoesArray, 0, numTransacoes - 1, compararDatasDecrescente, 2);
+
+    if (debitoEncontrado == NULL)
+    {
+        printf("Não há transações do tipo DÉBITO.\n");
+    }
+    else
+    {
+        int numero = 0;
+        for (int i = 0; i < numTransacoes; i++)
+        {
+            if (transacoesArray[i]->tipo == 2)
+            {
+                printf("======================================\n");
+                printf("      Transação nº%d (DÉBITO)\n", numero);
+                printf("Valor: R$%.2lf, Data: %.25s\n", transacoesArray[i]->valor, transacoesArray[i]->dataHora);
+                printf("Descrição: %s\n", transacoesArray[i]->descricao);
+                numero++;
+            }
+        }
+    }
+
+    free(transacoesArray);
 }
--- main.c
+++ main.c
@@ -3,38 +3,61 @@
 #include <string.h>
 #include "fila.h"
 #include <time.h>
+#include <stdbool.h>
 
-int main()
-{
+int main(){
     Fila *filalegal = criarFila();
 
+    int id =202300;
     int escolha = 0;
     double valor, saldo=0;
     char descricao[102];
     int tipo=0;
+    int numTransacoes = 0;
+
+    //adicionando transações exemplo"
+    /*adicionarTransacao(filalegal, criarTransacao(id, 12, 1, "01/08/2023 12:00:00", "sabonete"));
+    numTransacoes++;
+    id++;
+    saldo=saldo+12;
+    adicionarTransacao(filalegal, criarTransacao(id, 5.5, 2, "02/08/2023 12:00:00", "cerveja"));
+    numTransacoes++;
+    id++;
+    saldo=saldo-5.5;
+    adicionarTransacao(filalegal, criarTransacao(id, 100, 1, "03/08/2023 12:00:00", "quininha"));
+    numTransacoes++;
+    id++;
+    saldo=saldo+100;
+    adicionarTransacao(filalegal, criarTransacao(id, 300, 1, "04/08/2023 12:00:00", "achei na rua slc"));
+    numTransacoes++;
+    id++;
+    saldo=saldo+300;
+    adicionarTransacao(filalegal, criarTransacao(id, 250.5, 2, "05/08/2023 12:00:00", "blaze"));
+    numTransacoes++;
+    id++;
+    saldo=saldo-250.5;*/
 
 
     while (1)
     {
         system("clear");
         printf("Escolha uma opção:\n\n");
-        printf("1. MOSTRAR HISTÓRICO E SALDO (mais antigo até mais novo)\n");
-        printf("2. ADICIONAR TRANSAÇÃO\n");
-        printf("3. TIRAR ÚLTIMA TRANSAÇÃO\n");
-        printf("4. FECHAR PROGRAMA\n");
+        printf("1. ADICIONAR TRANSAÇÃO\n");
+        printf("2. MOSTRAR HISTÓRICO E SALDO (mais antigo até mais novo)\n");
+        printf("3. MOSTRAR HISTÓRICO E SALDO (mais novo até mais antigo)\n");
+        printf("4. TIRAR ÚLTIMA TRANSAÇÃO\n");
+        printf("5. MOSTRAR HISTÓRICO ORGANIZADO POR VALOR (maior para o menor)\n");
+        printf("6. MOSTRAR HISTÓRICO ORGANIZADO POR VALOR (menor para o maior)\n");
+        printf("7. MOSTRAR HISTÓRICO DE CRÉDITOS\n");
+        printf("8. MOSTRAR HISTÓRICO DE DÉBITOS\n");
+        printf("9. FECHAR PROGRAMA\n");
+
 
         scanf("%d", &escolha);
         system("clear");
         switch (escolha)
         {
         case 1:
-            printf("========================\nSaldo atual: R$%.2lf ||\n", saldo);
-            mostrarHistorico(filalegal);
-            printf("\nPressione Enter (talvez duas vezes) para retornar ao menu.\n");
-            getchar(); // Consumir o caractere de nova linha
-            getchar(); // Aguardar um novo caractere
-            break;
-        case 2:
             printf("Valor da transação: ");
             scanf("%lf", &valor);
             getchar(); // Consumir o caractere de nova linha
@@ -67,38 +90,109 @@ int main()
             printf("Descrição simples: ");
             fgets(descricao, sizeof(descricao), stdin);
             descricao[strlen(descricao) - 1] = '\0'; // Remover o '\n' do final
-            adicionarTransacao(filalegal, criarTransacao(valor, tipo, dataHora, descricao));
+            adicionarTransacao(filalegal, criarTransacao(id, valor, tipo, dataHora, descricao));
+            numTransacoes++;
+            id++;
             printf("Transação adicionada com sucesso! Pressione Enter para retornar.\n");
             break;
+        case 2: 
+            printf("========================\nSaldo atual: R$%.2lf ||\n", saldo);
+            mostrarHistorico(filalegal);
+            printf("\nPressione Enter (talvez duas vezes) para retornar ao menu.\n");
+            getchar(); // Aguardar um novo caractere
+            break;
+
+
         case 3:
         {
-            Transacao *transacaoRemovida = tirarTransacao(filalegal);
-            if (transacaoRemovida != NULL)
+            Transacao *transacoesArray[numTransacoes];
+            for (int i = 0; i < numTransacoes; i++)
             {
-                if(transacaoRemovida->tipo==1)
-                {
-                    saldo=saldo-transacaoRemovida->valor;
-                    printf("\nTransação de crédito removida. Dinheiro debitado.\n");
-                }
-                if(transacaoRemovida->tipo==2)
-                {
-                    saldo=saldo-transacaoRemovida->valor;
-                    printf("\nTransação de débito removida. Dinheiro estornado.\n");
-                }
-                printf("Valor: R$%.2lf, Data/Hora: %s\n", transacaoRemovida->valor, transacaoRemovida->dataHora);
-                printf("Descrição: %s\n", transacaoRemovida->descricao);
+                transacoesArray[i] = NULL;
+            }
 
-                free(transacaoRemovida);
-            }
-            else
+            Transacao *atual = filalegal->frente;
+            int i = 0;
+            while (atual != NULL)
             {
-                printf("Não há transações para remover.\n");
+                transacoesArray[i] = atual;
+                atual = atual->next;
+                i++;
             }
+
+            quicksortTransacoesPorData(transacoesArray, 0, numTransacoes - 1, compararDatasDecrescente);
+
+            printf("========================\nTransações Ordenadas por Data (mais novo -> mais antigo):\n");
+            for (int i = 0; i < numTransacoes; i++)
+            {
+                printf("======================================\n");
+                printf("      Transação nº%d (%s)\n", i+1, obterTipoTransacaoString(transacoesArray[i]->tipo));
+                printf("Valor: R$%.2lf, Data: %.25s\n", transacoesArray[i]->valor, transacoesArray[i]->dataHora);
+                printf("Descrição: %s\n", transacoesArray[i]->descricao);
+                printf("ID da Transação: #%d\n", transacoesArray[i]->id);
+            }
+            printf("========================\n");
+
             printf("\nPressione Enter (talvez duas vezes) para retornar ao menu.\n");
             getchar(); // Aguardar um novo caractere
         }
         break;
+
         case 4:
+        {
+            Transacao *transacaoRemovida = tirarTransacao(filalegal);
+            if (transacaoRemovida != NULL)
+            {
+                if (transacaoRemovida->tipo == 1)
+                {
+                    saldo -= transacaoRemovida->valor;
+                    printf("\nTransação de crédito removida. Dinheiro debitado.\n");
+                }
+                else if (transacaoRemovida->tipo == 2)
+                {
+                    saldo += transacaoRemovida->valor;
+                printf("\nTransação de débito removida. Dinheiro estornado.\n");
+                }
+
+            printf("Valor: R$%.2lf, Data/Hora: %s\n", transacaoRemovida->valor, transacaoRemovida->dataHora);
+            printf("Descrição: %s\n", transacaoRemovida->descricao);
+            numTransacoes--;
+            free(transacaoRemovida);
+            }   
+            else
+            {
+                printf("Não há transações para remover.\n");
+            }
+                printf("\nPressione Enter (talvez duas vezes) para retornar ao menu.\n");
+                getchar(); // Aguardar um novo caractere
+        }       
+        break;
+
+        case 5:
+            bubbleSortDescendingTransacoes(filalegal);
+            printf("\nPressione Enter (talvez duas vezes) para retornar ao menu.\n");
+            getchar(); // Aguardar um novo caractere
+        break;
+
+        case 6:
+            ordenarFila(filalegal, compararTransacoesCrescente);
+            printf("\nPressione Enter (talvez duas vezes) para retornar ao menu.\n");
+            getchar(); // Aguardar um novo caractere
+            break;
+
+        case 7:
+            mostrarTransacoesCredito(filalegal);
+            printf("\nPressione Enter para retornar ao menu.\n");
+            getchar();
+            getchar(); // Aguardar dois Enter para retornar ao menu
+            break;
+        case 8:
+            mostrarTransacoesDebito(filalegal);
+            printf("\nPressione Enter para retornar ao menu.\n");
+            getchar();
+            getchar(); // Aguardar dois Enter para retornar ao menu
+            break;
+        case 9:
             liberarFila(filalegal);
             printf("Programa encerrado.\n");
             return 0;
@@ -111,3 +205,5 @@ int main()
 
     return 0;
 }
+
+
